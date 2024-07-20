87d1503c27cbed064a7e287ce0cbddedd9d085c3
Amirabbas Jadidi <86429722+Amirabbasjadidi@users.noreply.github.com>
2024-07-20 17:39:46 +0330

    Add Main Files

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: [
+            "Python Programmer🐍",
+            "Web Designer💻",
+            "Telegram Bot Developer🚀👨‍💻",
+            "FOSS Enthusiast🔥",
+            "GNU/Linux Enthusiast🐧",
+            "AI and ML Enthusiast🤖",
+            "Open Source Contributor🌐",
+            "Counter Strike Player⌨️🖱️"
+        ],
+        typeSpeed: 50,
+        backSpeed: 50,
+        backDelay: 2000,
+        loop: true
+    };
+
+    var typed = new Typed("#typed-text", options);
+
+    const icons = document.querySelectorAll('.icon-link');
+
+    icons.forEach(icon => {
+        icon.addEventListener('mouseover', function() {
+            this.querySelector('img').style.transform = 'scale(1.2)';
+        });
+
+        icon.addEventListener('mouseout', function() {
+            this.querySelector('img').style.transform = 'scale(1)';
+        });
+
+        icon.addEventListener('mousedown', function() {
+            this.querySelector('img').style.transform = 'scale(1.1)';
+        });
+
+        icon.addEventListener('mouseup', function() {
+            this.querySelector('img').style.transform = 'scale(1.2)';
+        });
+    });
+
+    const discordLink = document.getElementById('discord-link');
+    const customDialog = document.getElementById('my-dialog');
+    const dialogText = document.getElementById('dialog-text');
+    const dialogOkButton = document.getElementById('dialog-ok-button');
+
+    discordLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        const discordId = 'amirabbas_jadidi';
+        navigator.clipboard.writeText(discordId).then(function() {
+            showDialog('Discord ID copied to clipboard!<br>Send a message to me :)');
+        }, function() {
+            showDialog('Failed to copy Discord ID. :(<br>Try again');
+        });
+    });
+
+    function showDialog(message) {
+        dialogText.innerHTML = message;
+        customDialog.classList.add('show');
+    }
+
+    dialogOkButton.addEventListener('click', function() {
+        customDialog.classList.remove('show');
+    });
+});
